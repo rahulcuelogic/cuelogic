8a9a6bafc780a65b3a2d2e4482abe48073803c17
rahulcuelogic <rahulb.cuelogic@gmail.com>
2013-08-21 17:07:37 +0530

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Cuelogic::Application.config.secret_key_base = 'bc713b22439c4b03b956b710b07e485206e35db5d017d7cf2de243cd05520ab54a31d41ab872a0d0ce7d216ae0d80ecd550e1d87e7cc804e159c74c5040c5898'
