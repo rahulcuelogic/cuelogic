54903fd068db908730492bdd575d8178e3bab873
rahulcuelogic <rahulb.cuelogic@gmail.com>
2013-08-21 19:56:28 +0530

    Finish layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Cuelogic::Application.routes.draw do
-  get "static_pages/home"
-  get "static_pages/help"
-  get "static_pages/about"
+  
+  root 'static_pages#home'
+  
+  get "users/new" => 'users#new'
+  get 'static_pages/help' => 'static_pages#help'
+  get 'static_pages/home' => 'static_pages#home'
+  get 'static_pages/about' => 'static_pages#about'
+  get 'static_pages/contact' => 'static_pages#contact'
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
