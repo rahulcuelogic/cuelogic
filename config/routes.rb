cc685d3a22c890a14fcef423dec70550781d803c
rahulcuelogic <rahulb.cuelogic@gmail.com>
2013-08-22 13:57:03 +0530

    Completed with layout and structure

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,21 @@ Cuelogic::Application.routes.draw do
   
   root 'static_pages#home'
   
+  match '/signup', to: 'users#new', via: 'get'
+
+
+  match '/help', to: 'static_pages#help', via: 'get'
+  match '/', to: 'static_pages#home', via: 'get'
+  match '/about', to: 'static_pages#about', via: 'get'
+  match '/contact', to: 'static_pages#contact', via: 'get'
+
+=begin
   get "users/new" => 'users#new'
   get 'static_pages/help' => 'static_pages#help'
   get 'static_pages/home' => 'static_pages#home'
   get 'static_pages/about' => 'static_pages#about'
   get 'static_pages/contact' => 'static_pages#contact'
+=end  
   
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
